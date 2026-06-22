406bd0158198198a08d8ac61ebb0f6beb24d71a2
Matt Milzewski <mattmilzewski@gmail.com>
2026-06-22 19:29:00 +1000

    Add Mower Man QLD forms configuration workflow

--- forms/terraform/variables.tf
+++ forms/terraform/variables.tf
@@ -21,7 +21,9 @@ variable "allowed_origins" {
   type        = list(string)
   default = [
     "https://www.anchorwebco.com.au",
-    "https://anchorwebco.com.au"
+    "https://anchorwebco.com.au",
+    "https://mowermanqld.com.au",
+    "https://www.mowermanqld.com.au"
   ]
 }
 
